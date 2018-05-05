0c2064dfb14f905ba73d02ce51a58309ecf427ea
Jovi <redzjovi@gmail.com>
2018-05-05 09:53:20 +0700

    Update MathHelper.
    - Add isBuzz.
    - Add isFizz.
    - Add isFizzBuzz.
    - Add isEven.
    - Add isOdd.
    - Add isPrime.

--- src/MathHelper.php
+++ src/MathHelper.php
@@ -4,6 +4,42 @@ namespace redzjovi\php;
 
 class MathHelper
 {
+    public static function isBuzz($number)
+    {
+        return ($number % 5 == 0) ? true : false;
+    }
+
+    public static function isFizz($number)
+    {
+        return ($number % 3 == 0) ? true : false;
+    }
+
+    public static function isFizzBuzz($number)
+    {
+        return ($number % 3 == 0 && $number % 5 == 0) ? true : false;
+    }
+
+    public static function isEven($number)
+    {
+        return ($number % 2 == 0) ? true : false;
+    }
+
+    public static function isOdd($number)
+    {
+        return ($number % 2 != 0) ? true : false;
+    }
+
+    public static function isPrime($number)
+    {
+        for ($i = 2; $i < $number; $i++) {
+            if ($number % $i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static function roundUp($number, $precisionDecimal)
     {
         $numberDecimal = $number - (int) $number;
